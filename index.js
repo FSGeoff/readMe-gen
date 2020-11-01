22ae3b9a5311b51d15eff47e23ae7dd9942eea18
Geoff Nix <FullStackGeoff@gmail.com>
2020-11-01 18:20:06 -0500

    added all README questions

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-console.log("Hello World");
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+inquirer
+	.prompt([
+		{
+			type: "input",
+			message: "What is the title of your README.md?",
+			name: "title",
+		},
+		{
+			type: "input",
+			message: "Please give a description of your project",
+			name: "description",
+		},
+		{
+			type: "input",
+			message: "Please give installation instructions",
+			name: "installation",
+		},
+		{
+			type: "input",
+			message: "Please give usage information",
+			name: "usage",
+		},
+		{
+			type: "input",
+			message: "Please list contribution guidelines",
+			name: "contributors",
+		},
+		{
+			type: "input",
+			message: "Please give testing instructions",
+			name: "testing",
+		},
+	])
+	.then((answers) => console.log("Hello World"));
+
+//     title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
